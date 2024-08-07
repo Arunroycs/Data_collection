5dec04bbb9f63f0ee52c8a4ffc24ae3d2bb76f2f
Arun Roy <arunroyd24@gmail.com>
2024-08-07 13:20:26 -0500

    Data_fetch

--- data_fetch.py
+++ data_fetch.py
@@ -1,4 +1,5 @@
 import requests
+from sqlalchemy import create_engine
 
 def fetch_api_data():
     url = 'https://jsonplaceholder.typicode.com/posts'
@@ -6,4 +7,31 @@ def fetch_api_data():
     data = response.json()
     return data
 
-fetch_api_data()
+import pandas as pd
+
+def cleanse_data(api_data):
+    # Convert API data to DataFrame
+    df_api = pd.DataFrame(api_data)
+    # Example cleansing: drop unnecessary columns
+    df_api_clean = df_api[['userId', 'id', 'title', 'body']]
+    
+    
+    return df_api_clean
+
+
+def store_data(df_api, df_web):
+    # Database connection string
+    DATABASE_URI = 'postgresql+psycopg2://data_user:password@localhost/data_collection_db'
+    engine = create_engine(DATABASE_URI)
+    
+    # Store API data
+    df_api.to_sql('api_data', engine, if_exists='replace', index=False)
+    
+    # Store web data
+    df_web.to_sql('web_data', engine, if_exists='replace', index=False)
+
+
+api_data = fetch_api_data()
+cleaned_data = cleanse_data(api_data)
+
+print(cleaned_data.head())
